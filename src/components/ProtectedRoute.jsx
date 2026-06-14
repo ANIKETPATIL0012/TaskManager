64384e6696669cb30db81bd43aeaed5fdd901e21
Aniket Patil <aniketpatilofficial12@gmail.com>
2026-06-14 13:52:47 +0530

    updated login and profile fix

--- src/components/ProtectedRoute.jsx
+++ src/components/ProtectedRoute.jsx
@@ -4,4 +4,4 @@ export default function ProtectedRoute({ children }) {
   const user = JSON.parse(localStorage.getItem("loggedInUser"));
 
   return user ? children : <Navigate to="/" />;
-git commit -m "updated login and profile fix"}
+}
